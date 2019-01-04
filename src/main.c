f9956ae234e5cb29a77438882ee7d5f155de7563
Nicolas Moutaud <nicolas.moutaud@epitech.eu>
2019-01-04 15:56:41 +0100

    [ADD] SIMPLE HELP
    - Add simple usage handling
    
    CHIP8 .

--- src/main.c
+++ src/main.c
@@ -1,3 +1,17 @@
-int main(void) {
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void dump_help(const char *name)
+{
+	printf("USAGE\n\t%s ROM_NAME\n", name);
+}
+
+int main(int ac, char **av)
+{
+	if (ac < 2 || !strcmp(av[1], "-h")) {
+		dump_help(av[0]);
+		return EXIT_SUCCESS;
+	}
+	return EXIT_SUCCESS;
 }
